6a44d2ee69ce72e27b340de122e138b070a5a334
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-03-23 22:28:10 -0400

    Respin Rust v1.41.1 to enable node variant. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 1.40/Dockerfile -t cimg/rust:1.40.0  -t cimg/rust:1.40 .
-docker build --file 1.40/node/Dockerfile -t cimg/rust:1.40.0-node  -t cimg/rust:1.40-node .
+docker build --file 1.41/Dockerfile -t cimg/rust:1.41.1  -t cimg/rust:1.41 .
+docker build --file 1.41/node/Dockerfile -t cimg/rust:1.41.1-node  -t cimg/rust:1.41-node .
