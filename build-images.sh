98c766ffda734390e5e14793f26a485af34b4352
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-04-24 13:47:44 -0400

    Add v1.43.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 1.42/Dockerfile -t cimg/rust:1.42.0  -t cimg/rust:1.42 .
-docker build --file 1.42/node/Dockerfile -t cimg/rust:1.42.0-node  -t cimg/rust:1.42-node .
+docker build --file 1.43/Dockerfile -t cimg/rust:1.43.0  -t cimg/rust:1.43 .
+docker build --file 1.43/node/Dockerfile -t cimg/rust:1.43.0-node  -t cimg/rust:1.43-node .
