2f1cadcc4acd511ac7aa8c41ec45c2a05974b50b
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-07-30 20:30:54 -0400

    Publish v1.45.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 1.45/Dockerfile -t cimg/rust:1.45.0  -t cimg/rust:1.45 .
-docker build --file 1.45/node/Dockerfile -t cimg/rust:1.45.0-node  -t cimg/rust:1.45-node .
+docker build --file 1.45/Dockerfile -t cimg/rust:1.45.1  -t cimg/rust:1.45 .
+docker build --file 1.45/node/Dockerfile -t cimg/rust:1.45.1-node  -t cimg/rust:1.45-node .
