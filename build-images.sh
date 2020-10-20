c11661165a2dc20b0f2207ba2a20925a99a42c02
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-10-19 23:52:27 -0400

    Add browser variant. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 1.47/Dockerfile -t cimg/rust:1.47.0  -t cimg/rust:1.47 .
-docker build --file 1.47/node/Dockerfile -t cimg/rust:1.47.0-node  -t cimg/rust:1.47-node .
-docker build --file 1.47/browsers/Dockerfile -t cimg/rust:1.47.0-browsers  -t cimg/rust:1.47-browsers .
+docker build --file 1.44/browsers/Dockerfile -t cimg/rust:1.44.1-browsers  -t cimg/rust:1.44-browsers .
+docker build --file 1.45/browsers/Dockerfile -t cimg/rust:1.45.2-browsers  -t cimg/rust:1.45-browsers .
+docker build --file 1.46/browsers/Dockerfile -t cimg/rust:1.46.0-browsers  -t cimg/rust:1.46-browsers .
