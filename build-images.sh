96e8f33459d816044ce1fc9de96e3db50f461d85
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-11-13 18:41:02 -0500

    Respin browsers tags for xvfb. [release] (#46)

--- build-images.sh
+++ build-images.sh
@@ -3,3 +3,4 @@
 docker build --file 1.44/browsers/Dockerfile -t cimg/rust:1.44.1-browsers  -t cimg/rust:1.44-browsers .
 docker build --file 1.45/browsers/Dockerfile -t cimg/rust:1.45.2-browsers  -t cimg/rust:1.45-browsers .
 docker build --file 1.46/browsers/Dockerfile -t cimg/rust:1.46.0-browsers  -t cimg/rust:1.46-browsers .
+docker build --file 1.47/browsers/Dockerfile -t cimg/rust:1.47.0-browsers  -t cimg/rust:1.47-browsers .
