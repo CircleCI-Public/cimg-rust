3a11816f1d7296c5f837f68df3257f48d7f81fde
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-11-20 14:53:29 -0500

    Publish v1.48.0. [release] (#47)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 1.44/browsers/Dockerfile -t cimg/rust:1.44.1-browsers  -t cimg/rust:1.44-browsers .
-docker build --file 1.45/browsers/Dockerfile -t cimg/rust:1.45.2-browsers  -t cimg/rust:1.45-browsers .
-docker build --file 1.46/browsers/Dockerfile -t cimg/rust:1.46.0-browsers  -t cimg/rust:1.46-browsers .
-docker build --file 1.47/browsers/Dockerfile -t cimg/rust:1.47.0-browsers  -t cimg/rust:1.47-browsers .
+docker build --file 1.48/Dockerfile -t cimg/rust:1.48.0  -t cimg/rust:1.48 .
+docker build --file 1.48/node/Dockerfile -t cimg/rust:1.48.0-node  -t cimg/rust:1.48-node .
+docker build --file 1.48/browsers/Dockerfile -t cimg/rust:1.48.0-browsers  -t cimg/rust:1.48-browsers .
