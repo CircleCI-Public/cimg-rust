92925d4e4c4f1bd0d23ee54500375e31ffc81d79
Adam Hartley <git@ahartley.com>
2021-05-10 16:14:40 +0100

    Publish v1.52.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 1.52/Dockerfile -t cimg/rust:1.52.0  -t cimg/rust:1.52 .
-docker build --file 1.52/node/Dockerfile -t cimg/rust:1.52.0-node  -t cimg/rust:1.52-node .
-docker build --file 1.52/browsers/Dockerfile -t cimg/rust:1.52.0-browsers  -t cimg/rust:1.52-browsers .
+docker build --file 1.52/Dockerfile -t cimg/rust:1.52.1  -t cimg/rust:1.52 .
+docker build --file 1.52/node/Dockerfile -t cimg/rust:1.52.1-node  -t cimg/rust:1.52-node .
+docker build --file 1.52/browsers/Dockerfile -t cimg/rust:1.52.1-browsers  -t cimg/rust:1.52-browsers .
