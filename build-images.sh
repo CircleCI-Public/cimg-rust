d20e9de2153b83832d2777af755c5ff11169f074
Michael Rothstein <michaelrothstein@Michaels-MacBook-Pro.local>
2021-07-30 08:47:29 -0700

    Publish v1.54.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 1.53/Dockerfile -t cimg/rust:1.53.0  -t cimg/rust:1.53 .
-docker build --file 1.53/node/Dockerfile -t cimg/rust:1.53.0-node  -t cimg/rust:1.53-node .
-docker build --file 1.53/browsers/Dockerfile -t cimg/rust:1.53.0-browsers  -t cimg/rust:1.53-browsers .
+docker build --file 1.54/Dockerfile -t cimg/rust:1.54.0  -t cimg/rust:1.54 .
+docker build --file 1.54/node/Dockerfile -t cimg/rust:1.54.0-node  -t cimg/rust:1.54-node .
+docker build --file 1.54/browsers/Dockerfile -t cimg/rust:1.54.0-browsers  -t cimg/rust:1.54-browsers .
