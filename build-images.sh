2be0dc64cbffdc7300beb0619b6c8aeba6fe644f
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-10-21 12:14:02 -0400

    Publish v1.56.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 1.55/Dockerfile -t cimg/rust:1.55.0  -t cimg/rust:1.55 .
-docker build --file 1.55/node/Dockerfile -t cimg/rust:1.55.0-node  -t cimg/rust:1.55-node .
-docker build --file 1.55/browsers/Dockerfile -t cimg/rust:1.55.0-browsers  -t cimg/rust:1.55-browsers .
+docker build --file 1.56/Dockerfile -t cimg/rust:1.56.0  -t cimg/rust:1.56 .
+docker build --file 1.56/node/Dockerfile -t cimg/rust:1.56.0-node  -t cimg/rust:1.56-node .
+docker build --file 1.56/browsers/Dockerfile -t cimg/rust:1.56.0-browsers  -t cimg/rust:1.56-browsers .
