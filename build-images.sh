f396590ace9c9ab598ed78a1ddd040b7f84bd9c6
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-01-20 18:13:14 -0500

    Publish v1.58.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.58/Dockerfile -t cimg/rust:1.58.0  -t cimg/rust:1.58 .
-docker build --file 1.58/node/Dockerfile -t cimg/rust:1.58.0-node  -t cimg/rust:1.58-node .
-docker build --file 1.58/browsers/Dockerfile -t cimg/rust:1.58.0-browsers  -t cimg/rust:1.58-browsers .
+docker build --file 1.58/Dockerfile -t cimg/rust:1.58.1  -t cimg/rust:1.58 .
+docker build --file 1.58/node/Dockerfile -t cimg/rust:1.58.1-node  -t cimg/rust:1.58-node .
+docker build --file 1.58/browsers/Dockerfile -t cimg/rust:1.58.1-browsers  -t cimg/rust:1.58-browsers .
---
+++ push-images.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
+
+docker push cimg/rust:1.58.1
+docker push cimg/rust:1.58
+docker push cimg/rust:1.58.1-node
+docker push cimg/rust:1.58-node
+docker push cimg/rust:1.58.1-browsers
+docker push cimg/rust:1.58-browsers
