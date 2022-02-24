68e9416cdd05192495d942e8cc92c4adda06a4bd
Jalexchen <jeff.chen@circleci.com>
2022-02-24 13:40:25 -0800

    Publish v1.59.0. [release] (#75)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.58/Dockerfile -t cimg/rust:1.58.1  -t cimg/rust:1.58 .
-docker build --file 1.58/node/Dockerfile -t cimg/rust:1.58.1-node  -t cimg/rust:1.58-node .
-docker build --file 1.58/browsers/Dockerfile -t cimg/rust:1.58.1-browsers  -t cimg/rust:1.58-browsers .
+docker build --file 1.59/Dockerfile -t cimg/rust:1.59.0  -t cimg/rust:1.59 .
+docker build --file 1.59/node/Dockerfile -t cimg/rust:1.59.0-node  -t cimg/rust:1.59-node .
+docker build --file 1.59/browsers/Dockerfile -t cimg/rust:1.59.0-browsers  -t cimg/rust:1.59-browsers .
