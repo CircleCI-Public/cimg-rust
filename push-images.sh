2c145b2301180fea22c0c64ef059d6c48a502ead
Jeff Chen <jeff.chen@circleci.com>
2022-02-24 09:46:11 -0800

    Publish v1.59.0. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/rust:1.58.1
-docker push cimg/rust:1.58
-docker push cimg/rust:1.58.1-node
-docker push cimg/rust:1.58-node
-docker push cimg/rust:1.58.1-browsers
-docker push cimg/rust:1.58-browsers
+docker push cimg/rust:1.59.0
+docker push cimg/rust:1.59
+docker push cimg/rust:1.59.0-node
+docker push cimg/rust:1.59-node
+docker push cimg/rust:1.59.0-browsers
+docker push cimg/rust:1.59-browsers
