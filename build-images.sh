5d6b4b7cb53a7754cbd36778425cc007181056d9
Jalexchen <jeff.chen@circleci.com>
2022-04-07 14:05:45 -0700

    Publish v1.60.0. [release] (#77)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.59/Dockerfile -t cimg/rust:1.59.0  -t cimg/rust:1.59 .
-docker build --file 1.59/node/Dockerfile -t cimg/rust:1.59.0-node  -t cimg/rust:1.59-node .
-docker build --file 1.59/browsers/Dockerfile -t cimg/rust:1.59.0-browsers  -t cimg/rust:1.59-browsers .
+docker build --file 1.60/Dockerfile -t cimg/rust:1.60.0  -t cimg/rust:1.60 .
+docker build --file 1.60/node/Dockerfile -t cimg/rust:1.60.0-node  -t cimg/rust:1.60-node .
+docker build --file 1.60/browsers/Dockerfile -t cimg/rust:1.60.0-browsers  -t cimg/rust:1.60-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/rust:1.59.0
-docker push cimg/rust:1.59
-docker push cimg/rust:1.59.0-node
-docker push cimg/rust:1.59-node
-docker push cimg/rust:1.59.0-browsers
-docker push cimg/rust:1.59-browsers
+docker push cimg/rust:1.60.0
+docker push cimg/rust:1.60
+docker push cimg/rust:1.60.0-node
+docker push cimg/rust:1.60-node
+docker push cimg/rust:1.60.0-browsers
+docker push cimg/rust:1.60-browsers
