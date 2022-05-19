d3c30575b467159cd1a6702f949b8f391812fdac
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-05-19 16:00:26 +0100

    Publish v1.61.0. [release] (#78)
    * Publish v1.61.0. [release]
    
    * Bump base image
    
    Co-authored-by: Ricardo N Feliciano <FelicianoTech@gmail.com>

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.60/Dockerfile -t cimg/rust:1.60.0  -t cimg/rust:1.60 .
-docker build --file 1.60/node/Dockerfile -t cimg/rust:1.60.0-node  -t cimg/rust:1.60-node .
-docker build --file 1.60/browsers/Dockerfile -t cimg/rust:1.60.0-browsers  -t cimg/rust:1.60-browsers .
+docker build --file 1.61/Dockerfile -t cimg/rust:1.61.0  -t cimg/rust:1.61 .
+docker build --file 1.61/node/Dockerfile -t cimg/rust:1.61.0-node  -t cimg/rust:1.61-node .
+docker build --file 1.61/browsers/Dockerfile -t cimg/rust:1.61.0-browsers  -t cimg/rust:1.61-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/rust:1.60.0
-docker push cimg/rust:1.60
-docker push cimg/rust:1.60.0-node
-docker push cimg/rust:1.60-node
-docker push cimg/rust:1.60.0-browsers
-docker push cimg/rust:1.60-browsers
+docker push cimg/rust:1.61.0
+docker push cimg/rust:1.61
+docker push cimg/rust:1.61.0-node
+docker push cimg/rust:1.61-node
+docker push cimg/rust:1.61.0-browsers
+docker push cimg/rust:1.61-browsers
