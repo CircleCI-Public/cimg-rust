fb5ae3ef4855c8cd9d19f0763b6e6ec8508ed3e6
makotia <3mdevmail@gmail.com>
2022-07-15 00:27:05 +0900

    Publish v1.62.0. [release] (#79)
    * Publish v1.62.0. [release]
    
    * update base to 22.06 [release]
    
    Co-authored-by: Jeff Chen <jeff.chen@circleci.com>

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.61/Dockerfile -t cimg/rust:1.61.0  -t cimg/rust:1.61 .
-docker build --file 1.61/node/Dockerfile -t cimg/rust:1.61.0-node  -t cimg/rust:1.61-node .
-docker build --file 1.61/browsers/Dockerfile -t cimg/rust:1.61.0-browsers  -t cimg/rust:1.61-browsers .
+docker build --file 1.62/Dockerfile -t cimg/rust:1.62.0  -t cimg/rust:1.62 .
+docker build --file 1.62/node/Dockerfile -t cimg/rust:1.62.0-node  -t cimg/rust:1.62-node .
+docker build --file 1.62/browsers/Dockerfile -t cimg/rust:1.62.0-browsers  -t cimg/rust:1.62-browsers .
