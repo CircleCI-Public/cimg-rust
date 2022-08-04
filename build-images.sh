b7d8aa19961a07185c53c2c7618897520b3c27c2
Jeff Chen <jeff.chen@circleci.com>
2022-08-04 13:44:12 -0700

    Publish v1.62.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.62/Dockerfile -t cimg/rust:1.62.0  -t cimg/rust:1.62 .
-docker build --file 1.62/node/Dockerfile -t cimg/rust:1.62.0-node  -t cimg/rust:1.62-node .
-docker build --file 1.62/browsers/Dockerfile -t cimg/rust:1.62.0-browsers  -t cimg/rust:1.62-browsers .
+docker build --file 1.62/Dockerfile -t cimg/rust:1.62.1  -t cimg/rust:1.62 .
+docker build --file 1.62/node/Dockerfile -t cimg/rust:1.62.1-node  -t cimg/rust:1.62-node .
+docker build --file 1.62/browsers/Dockerfile -t cimg/rust:1.62.1-browsers  -t cimg/rust:1.62-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/rust:1.62.0
+docker push cimg/rust:1.62.1
 docker push cimg/rust:1.62
-docker push cimg/rust:1.62.0-node
+docker push cimg/rust:1.62.1-node
 docker push cimg/rust:1.62-node
-docker push cimg/rust:1.62.0-browsers
+docker push cimg/rust:1.62.1-browsers
 docker push cimg/rust:1.62-browsers
