07d06ab5b0d931d2fc110721f29887c23ff7a373
makotia <3mdevmail@gmail.com>
2022-09-22 23:49:15 +0900

    Publish v1.64.0. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/rust:1.63.0
-docker push cimg/rust:1.63
-docker push cimg/rust:1.63.0-node
-docker push cimg/rust:1.63-node
-docker push cimg/rust:1.63.0-browsers
-docker push cimg/rust:1.63-browsers
+docker push cimg/rust:1.64.0
+docker push cimg/rust:1.64
+docker push cimg/rust:1.64.0-node
+docker push cimg/rust:1.64-node
+docker push cimg/rust:1.64.0-browsers
+docker push cimg/rust:1.64-browsers
