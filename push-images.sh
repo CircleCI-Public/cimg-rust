1f3f854007733fea0ec1892d8d95f9d2ec8660d5
Jalexchen <jeff.chen@circleci.com>
2022-10-05 13:42:13 -0700

    updates dockerfile and generates gen-check (#86)

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-
-docker push cimg/rust:1.64.0
 docker push cimg/rust:1.64
-docker push cimg/rust:1.64.0-node
+docker push cimg/rust:1.64.0
 docker push cimg/rust:1.64-node
-docker push cimg/rust:1.64.0-browsers
+docker push cimg/rust:1.64.0-node
 docker push cimg/rust:1.64-browsers
+docker push cimg/rust:1.64.0-browsers
