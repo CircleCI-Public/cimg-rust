8e1d439b70fae45a6833caea3dd3d9e19aa9979b
Conrad Ludgate <conrad.ludgate@truelayer.com>
2022-11-04 15:45:47 +0000

    release-v1.65.0 with november base

--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/rust:1.64
-docker push cimg/rust:1.64.0
-docker push cimg/rust:1.64-node
-docker push cimg/rust:1.64.0-node
-docker push cimg/rust:1.64-browsers
-docker push cimg/rust:1.64.0-browsers
+docker push cimg/rust:1.65
+docker push cimg/rust:1.65.0
+docker push cimg/rust:1.65-node
+docker push cimg/rust:1.65.0-node
+docker push cimg/rust:1.65-browsers
+docker push cimg/rust:1.65.0-browsers
