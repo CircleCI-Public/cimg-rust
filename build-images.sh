996ff722338a48655f80a68c7f8e367900fdb6a5
Jeff Chen <jeff.chen@circleci.com>
2022-12-20 12:12:40 -0800

    Publish v1.66.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.65/Dockerfile -t cimg/rust:1.65.0 -t cimg/rust:1.65 .
-docker build --file 1.65/node/Dockerfile -t cimg/rust:1.65.0-node -t cimg/rust:1.65-node .
-docker build --file 1.65/browsers/Dockerfile -t cimg/rust:1.65.0-browsers -t cimg/rust:1.65-browsers .
+docker build --file 1.66/Dockerfile -t cimg/rust:1.66.0 -t cimg/rust:1.66 .
+docker build --file 1.66/node/Dockerfile -t cimg/rust:1.66.0-node -t cimg/rust:1.66-node .
+docker build --file 1.66/browsers/Dockerfile -t cimg/rust:1.66.0-browsers -t cimg/rust:1.66-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/rust:1.65
-docker push cimg/rust:1.65.0
-docker push cimg/rust:1.65-node
-docker push cimg/rust:1.65.0-node
-docker push cimg/rust:1.65-browsers
-docker push cimg/rust:1.65.0-browsers
+docker push cimg/rust:1.66
+docker push cimg/rust:1.66.0
+docker push cimg/rust:1.66-node
+docker push cimg/rust:1.66.0-node
+docker push cimg/rust:1.66-browsers
+docker push cimg/rust:1.66.0-browsers
