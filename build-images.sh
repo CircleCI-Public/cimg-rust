bba1b4f04ceecd17070f9645ebb0885f0c03b868
Adam Hartley <git@ahartley.com>
2023-01-17 16:02:00 +0000

    Publish v1.66.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.66/Dockerfile -t cimg/rust:1.66.0 -t cimg/rust:1.66 .
-docker build --file 1.66/node/Dockerfile -t cimg/rust:1.66.0-node -t cimg/rust:1.66-node .
-docker build --file 1.66/browsers/Dockerfile -t cimg/rust:1.66.0-browsers -t cimg/rust:1.66-browsers .
+docker build --file 1.66/Dockerfile -t cimg/rust:1.66.1 -t cimg/rust:1.66 .
+docker build --file 1.66/node/Dockerfile -t cimg/rust:1.66.1-node -t cimg/rust:1.66-node .
+docker build --file 1.66/browsers/Dockerfile -t cimg/rust:1.66.1-browsers -t cimg/rust:1.66-browsers .
