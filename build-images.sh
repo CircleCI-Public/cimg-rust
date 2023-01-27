1267b954b2dce89d83f6deda92755159e6b7405c
Jeff Chen <jeff.chen@circleci.com>
2023-01-27 11:22:53 -0800

    Publish v1.67.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.66/Dockerfile -t cimg/rust:1.66.1 -t cimg/rust:1.66 .
-docker build --file 1.66/node/Dockerfile -t cimg/rust:1.66.1-node -t cimg/rust:1.66-node .
-docker build --file 1.66/browsers/Dockerfile -t cimg/rust:1.66.1-browsers -t cimg/rust:1.66-browsers .
+docker build --file 1.67/Dockerfile -t cimg/rust:1.67.0 -t cimg/rust:1.67 .
+docker build --file 1.67/node/Dockerfile -t cimg/rust:1.67.0-node -t cimg/rust:1.67-node .
+docker build --file 1.67/browsers/Dockerfile -t cimg/rust:1.67.0-browsers -t cimg/rust:1.67-browsers .
