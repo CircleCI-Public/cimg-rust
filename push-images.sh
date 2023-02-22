b1a0f3adc64647d23bbfd8711b3e799136ab5b93
Jalexchen <jeff.chen@circleci.com>
2023-02-21 16:41:16 -0800

    Publish v1.67.1. [release] (#99)

--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push cimg/rust:1.67
-docker push cimg/rust:1.67.0
+docker push cimg/rust:1.67.1
 docker push cimg/rust:1.67-node
-docker push cimg/rust:1.67.0-node
+docker push cimg/rust:1.67.1-node
 docker push cimg/rust:1.67-browsers
-docker push cimg/rust:1.67.0-browsers
+docker push cimg/rust:1.67.1-browsers
