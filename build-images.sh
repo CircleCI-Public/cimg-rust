4013da7ca80d746062b9cbac1c5363b7ebeb7954
Adam Hartley <git@ahartley.com>
2023-07-19 10:12:40 +0100

    Publish v1.71.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+set -eo pipefail
 
-docker build --file 1.71/Dockerfile -t cimg/rust:1.71.0 -t cimg/rust:1.71 --platform linux/amd64 .
-docker build --file 1.71/node/Dockerfile -t cimg/rust:1.71.0-node -t cimg/rust:1.71-node --platform linux/amd64 .
-docker build --file 1.71/browsers/Dockerfile -t cimg/rust:1.71.0-browsers -t cimg/rust:1.71-browsers --platform linux/amd64 .
+docker context create cimg
+docker buildx create --use cimg
+docker buildx build --platform=linux/amd64,linux/arm64 --file 1.71/Dockerfile -t cimg/rust:1.71.0 -t cimg/rust:1.71 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 1.71/node/Dockerfile -t cimg/rust:1.71.0-node -t cimg/rust:1.71-node --push .
+docker buildx build --platform=linux/amd64 --file 1.71/browsers/Dockerfile -t cimg/rust:1.71.0-browsers -t cimg/rust:1.71-browsers --push .
