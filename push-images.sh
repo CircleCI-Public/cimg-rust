e7e1ddfed579bf23f82ad999b0851136c1d742fc
Adam Hartley <BytesGuy@users.noreply.github.com>
2023-07-19 10:23:17 +0100

    Publish v1.71.0. [release] (#113)

--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/rust:1.71
-docker push cimg/rust:1.71.0
-docker push cimg/rust:1.71-node
-docker push cimg/rust:1.71.0-node
-docker push cimg/rust:1.71-browsers
-docker push cimg/rust:1.71.0-browsers
+set -eo pipefail
